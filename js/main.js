a90b23bf12a2069033d5b0dca86626adf9054d34
Sergey <sersergius@gmail.com>
2020-07-20 21:25:52 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,37 +1,53 @@
 // ------------------------------------------
-let str1 = 'qwe_asd zxc/123 !@#';
+let str1 = 'qwe_asd zxc/////123 !@#';
 let str2 = 'vbn fgh_rty 456 $%^';
-let str3 = 'nm, jkl/uio 789_&*(';
+let str3 = 'nm, jkl/uio //789_&*(';
 let restrictedList = ['!', ' ', '_', '/'];
 
-let clear1 = str1.split('');
-let clear2 = str2.split('');
-let clear3 = str3.split('');
-console.log('Start1: ' + clear1);
-console.log('Start2: ' + clear2);
-console.log('Start3: ' + clear3);
+str1 = str1.split('');
+str2 = str2.split('');
+str3 = str3.split('');
+console.log('Start1: ' + str1);
+console.log('Start2: ' + str2);
+console.log('Start3: ' + str3);
+let clear1 = [];
+let clear2 = [];
+let clear3 = [];
+let test = null;
 
-for (let i=0; i<restrictedList.length; i++) {
-    for (let j=0; j<clear1.length; j++) {
-        if (restrictedList[i] === clear1[j]) {
-            clear1.splice(j,1);
-        }
+for (let i=0; i<str1.length; i++) {
+    for (let j=0; j<restrictedList.length; j++) {
+        if (str1[i] === restrictedList[j]) {
+            test = true;
+            break;
+        } test = false;
+    }
+    if (!test) {
+        clear1.push(str1[i]);
     }
 }
 
-for (let i=0; i<restrictedList.length; i++) {
-    for (let j=0; j<clear2.length; j++) {
-        if (restrictedList[i] === clear2[j]) {
-            clear2.splice(j,1);
-        }
+for (let i=0; i<str2.length; i++) {
+    for (let j=0; j<restrictedList.length; j++) {
+        if (str1[i] === restrictedList[j]) {
+            test = true;
+            break;
+        } test = false;
+    }
+    if (!test) {
+        clear2.push(str2[i]);
     }
 }
 
-for (let i=0; i<restrictedList.length; i++) {
-    for (let j=0; j<clear3.length; j++) {
-        if (restrictedList[i] === clear3[j]) {
-            clear3.splice(j,1);
-        }
+for (let i=0; i<str3.length; i++) {
+    for (let j=0; j<restrictedList.length; j++) {
+        if (str3[i] === restrictedList[j]) {
+            test = true;
+            break;
+        } test = false;
+    }
+    if (!test) {
+        clear3.push(str3[i]);
     }
 }
 
